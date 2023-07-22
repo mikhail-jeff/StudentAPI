797603a9802a02ef2987cc3c499e85453ddf683f
jeffmikhail <mikhailcruz78@gmail.com>
2023-07-22 12:46:18 +0800

    Add project files.

---
+++ StudentAPI/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        //[HttpGet]
+        //public string Greetings()
+        //{
+        //    return "Hello World...";
+        //}
+
+        //[HttpGet]
+        //public IActionResult Goodnight()
+        //{
+        //    return Ok("night night motherfucker");
+
+        //    return NoContent();
+        //}
+
+        [HttpGet]
+        public ActionResult<string> Greet()
+        {
+            return "Hello World...";
+        }
+    }
+}
+ 
